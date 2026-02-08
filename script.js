8715db13c28bc802854541b70f418fdb26acb495
bayabongadlamini <mj@fourthict.co.za>
2026-02-08 17:24:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+﻿window.onload = function () {
+    loadTasks();
+};
+
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let taskText = input.value;
+
+    if (taskText === "") {
+        alert("Please enter a task");
+        return;
+    }
+
+    let tasks = getTasks();
+
+    tasks.push({
+        text: taskText,
+        completed: false
+    });
+
+    saveTasks(tasks);
+    input.value = "";
+    loadTasks();
+}
+
+function loadTasks() {
+    let taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+
+    let tasks = getTasks();
+
+    tasks.forEach(function (task, index) {
+        let li = document.createElement("li");
+
+        let span = document.createElement("span");
+        span.textContent = task.text;
+
+        if (task.completed) {
+            span.classList.add("completed");
+        }
+
+        span.onclick = function () {
+            toggleTask(index);
+        };
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "X";
+        deleteBtn.className = "delete-btn";
+
+        deleteBtn.onclick = function (event) {
+            event.stopPropagation(); // stops toggle
+            deleteTask(index);
+        };
+
+        li.appendChild(span);
+        li.appendChild(deleteBtn);
+        taskList.appendChild(li);
+    });
+}
+
+function toggleTask(index) {
+    let tasks = getTasks();
+    tasks[index].completed = !tasks[index].completed;
+    saveTasks(tasks);
+    loadTasks();
+}
+
+function deleteTask(index) {
+    let tasks = getTasks();
+    tasks.splice(index, 1);
+    saveTasks(tasks);
+    loadTasks();
+}
+
+function clearAll() {
+    if (confirm("Clear all tasks?")) {
+        localStorage.removeItem("tasks");
+        loadTasks();
+    }
+}
+
+function getTasks() {
+    let tasks = localStorage.getItem("tasks");
+    return tasks ? JSON.parse(tasks) : [];
+}
+
+function saveTasks(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
